bf30c30895c205bb896d97e77b936a2c0c7cbdec
Naomi <nani.torres.serrano@outlook.com>
2021-06-21 14:46:16 -0400

    campleted table/ the left joints

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS EmployeeTracker;
+
+CREATE DATABASE EmployeeTracker;
+
+USE EmployeeTracker;
+
+CREATE TABLE department (
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    role_id int,
+    is_manager boolean,
+    manager_id int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id int NOT NULL AUTO_INCREMENT,
+    title varchar(30) NOT NULL,
+    salary decimal(10,2) NOT NULL,
+    department_id int,
+    PRIMARY KEY (id)
+);
+
+    -- FOREIGN KEY (role_id) REFERENCES Role (id),
+    -- FOREIGN KEY (department_id) REFERENCES department (id),
---
+++ seed.sql
@@ -0,0 +1,47 @@
+USE EmployeeTracker;
+
+INSERT INTO employee (first_name, last_name, role_id, is_manager, manager_id)
+VALUES ("Adriel", "Garcia", 8, 1, null),
+("Joelys", "Gracia", 7, 1, 10),
+("Daisy", "Padilla", 6, 0, 6),
+("Alberto", "Serrano", 6, 1, 10),
+("Angelica", "Serrano", 5, 0, null),
+("Carlos", "Serrano", 5, 1, 10),
+("Kevin", "Serrano", 6, 0, 6),
+("Kenny", "Serrano", 7, 1, 6),
+("Keysha", "Miranda", 2, 0, 1),
+("Naomi", "Torres", 1, 1, 10);
+
+SELECT * FROM employee;
+
+INSERT INTO department (name)
+VALUES ("Engineering"), 
+("Finance"),
+("Legal"),
+("Sales");
+
+SELECT * FROM department;
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4), 
+("Salesperson", 80000, 4),
+("Legal Team Lead", 250000, 3),
+("Lawyer", 190000, 3),
+("Lead Engineer", 150000, 1),
+("Software Engineer", 120000, 1),
+("Accountant", 125000, 2),
+("Project Manager", 250000, 1);
+
+SELECT * FROM role;
+
+
+SELECT e.id "ID", e.first_name "First Name", e.last_name "Last Name", r.title "Title", d.name "Department", r.salary "Salary", 
+CONCAT (m.first_name, " ", m.last_name) "Manager" 
+FROM employee e
+LEFT JOIN role r
+ON r.id = e.role_id
+LEFT JOIN department d
+ON d.id = r.department_id
+LEFT JOIN employee m
+ON m.id = e.manager_id
+ORDER BY id;
